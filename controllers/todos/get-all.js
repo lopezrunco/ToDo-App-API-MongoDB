01914ce023481adb586412b98f329f8a2ee7f151
lopezrunco <lopezrunco@gmail.com>
2021-11-05 20:11:28 -0300

    Listado de tareas

--- controllers/todos/get-all.js
+++ controllers/todos/get-all.js
@@ -1,40 +1,44 @@
 const todoModel = require('../../models/todo')
 
-module.exports = (sequelize) => {
-    return (request, response) => {
+module.exports = (request, response) => {
 
-        // Definicion de un objeto vacio para la paginacion
-        const pagination = {}
+    // Definicion de una paginacion por defecto
+    let pagination = {
+        offset: 0,
+        limit: 10
+    }
 
-        // Este codigo correra solo si se estan mandando los parametros page & itemsPerPage
-        if (request.query.page && request.query.itemsPerPage) {
-
-            // Definicion de offset (items que se saltaran al mostrar) 
-            pagination.offset = (request.query.page - 1) * request.query.itemsPerPage
-            // Definicion de items a mostrar por pagina
+    // Si vienen valores asignaos por el usuario, los asignamos a la paginacion, si no, se asignan los valores por defecto
+    if (request.query.page && request.query.itemsPerPage) {
+        pagination.offset = (request.query.page - 1) * request.query.itemsPerPage,
             pagination.limit = parseInt(request.query.itemsPerPage)
-        }
+    }
 
-        // Lista todos las tareas y responde mostrandolas en la UI
-        todoModel(sequelize).findAll(pagination).then(todos => {
-            // El metodo count cuenta la cantidad de objetos
-            todoModel(sequelize).count().then(count => {
-                const meta = {
-                    count
-                }
+    // Lista todos las tareas y responde mostrandolas en la UI
+    todoModel
+        .find()
+        .skip(pagination.offset)
+        .limit(pagination.limit)
+        .then(todos => {
+            todoModel.
+                count()
+                .then(count => {
+                    const meta = {
+                        count
+                    }
 
-                // Responde al front end con el numero de tareas y las tareas en si
-                response.status(200).json({
-                    meta,
-                    todos
+                    // Responde al front end con el numero de tareas y las tareas en si
+                    response.status(200).json({
+                        meta,
+                        todos
+                    })
+                }).catch(error => {
+                    console.error(error)
+
+                    response.status(500).json({
+                        message: 'Error al intentar listar las tareas'
+                    })
                 })
-            }).catch(error => {
-                console.error(error)
-    
-                response.status(500).json({
-                    message: 'Error al intentar listar las tareas'
-                })
-            })
 
         }).catch(error => {
             console.error(error)
@@ -43,5 +47,4 @@ module.exports = (sequelize) => {
                 message: 'Error al intentar listar las tareas'
             })
         })
-    }
 }
