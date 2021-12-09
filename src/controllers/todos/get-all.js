b5d9f705d834d0837c8087f0af6884852946f1eb
lopezrunco <lopezrunco@gmail.com>
2021-12-09 15:40:20 -0300

    Count en listado de todos con Aggregate

--- src/controllers/todos/get-all.js
+++ src/controllers/todos/get-all.js
@@ -1,44 +1,37 @@
+const mongoose = require('mongoose')                // Lo usaremos para el ObjectId
 const { userModel } = require('../../models/user')
 
 module.exports = (request, response) => {
-
-    // Definicion de una paginacion por defecto
-    const pagination = {
-        offset: 0,
-        limit: 10
-    }
-    // Si vienen valores asignados por el usuario, los asignamos a la paginacion, si no, se asignan los valores por defecto
-    if (request.query.page && request.query.itemsPerPage) {
-        pagination.offset = (request.query.page - 1) * request.query.itemsPerPage,
-        pagination.limit = parseInt(request.query.itemsPerPage)
-    }
-
-    // Lista todas las tareas del usuario logueado en la UI
     userModel
-        .findOne({ _id: request.user.id })    // Accede al usuario segun el ID inyectado en el middleware checkIfTheUserHasCredentials
-        .select('todos')                  // Para el usuario logueado, selecciona los todos
-        .skip(pagination.offset)            // Aplica paginacion
-        .limit(pagination.limit)
+        .findOne({ _id: request.user.id })  // Accede al usuario segun el ID inyectado en el middleware checkIfTheUserHasCredentials
+        .select('todos')                    // Para el usuario logueado, selecciona los todos
         .then(user => {
-            userModel
-                .count()                     // TODO: Para este usuario en particular, contar el largo del array de tareas
-                .then(count => {
-                    const meta = {
-                        count
-                    }
+            // Agregate es una consulta que puede tener varias etapas
+            userModel.aggregate([{
+                $match: { _id: mongoose.Types.ObjectId(request.user.id) }   // $match es un equivalente de find. Types.ObjectId obitne el ID de un objeto a partir de un string
+            }, {
+                $project: {     // Hace una proyeccion, o sea, agregar un campo. En este caso es $size (como un lenght) de las todos
+                    count: { $size: '$todos' }
+                }
+            }]).then(countAggregation => {
+                // Se accede al count y se lo muestra
+                const count = countAggregation[0].count
+                const meta = {
+                    count
+                }
 
-                    // Responde al front end con el numero de tareas del usuario y las tareas en si
-                    response.status(200).json({
-                        meta,
-                        todos: user.todos
-                    })
-                }).catch(error => {
-                    console.error(error)
-
-                    response.status(500).json({
-                        message: 'Error al intentar listar las tareas'
-                    })
+                // Responde al front end con el numero de tareas del usuario y las tareas
+                response.status(200).json({
+                    meta,
+                    todos: user.todos
                 })
+            }).catch(error => {
+                console.error(error)
+
+                response.status(500).json({
+                    message: 'Error al intentar listar las tareas'
+                })
+            })
 
         }).catch(error => {
             console.error(error)
